ae4c98334d7ea14f118b48bd451f762a455b62c8
Mario Martelli <cocoa@schnuddelhuddel.de>
2017-11-25 19:01:24 +0100

    Garmin XSLT is used for exported gpx file
    Fixes #2

--- GpxExport/WorkoutsTableViewController.swift
+++ GpxExport/WorkoutsTableViewController.swift
@@ -125,15 +125,15 @@ class WorkoutsTableViewController: UITableViewController {
         return
       }
 
-      let iso_formatter = ISO8601DateFormatter()
       var current_heart_rate_index = 0
       var current_hr: Double = -1
       let bpm_unit = HKUnit(from: "count/min")
       var hr_string = ""
-      file.write(
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><gpx version=\"1.1\" creator=\"Apple Workouts (via pilif's hack of the week)\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.topografix.com/GPX/1/1\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\" xmlns:gpxtpx=\"http://www.garmin.com/xmlschemas/TrackPointExtension/v1\"><trk><name><![CDATA[\(workout_title)]]></name><time>\(iso_formatter.string(from: workout.startDate))</time><trkseg>"
-          .data(using: .utf8)!
-      )
+      if let header = self.gpxHeader(title: workout_title, startDate: workout.startDate).data(using: .utf8) {
+        file.write(
+            header
+        )
+      }
 
       self.workoutStore.route(for: workouts[indexPath.row]){
         (maybe_locations, done, error) in
@@ -147,16 +147,19 @@ class WorkoutsTableViewController: UITableViewController {
           while (current_heart_rate_index < keyedRates.count) && (location.timestamp > keyedRates[current_heart_rate_index].startDate) {
             current_hr = keyedRates[current_heart_rate_index].quantity.doubleValue(for: bpm_unit)
             current_heart_rate_index += 1
-            hr_string = "<extensions><gpxtpx:TrackPointExtension><gpxtpx:hr>\(current_hr)</gpxtpx:hr></gpxtpx:TrackPointExtension></extensions>"
+            hr_string = self.gpxHeartRate(current_hr: current_hr)
+          }
+          if let trackpoint = self.gpxTrackPoint(location: location, hr_string: hr_string).data(using: .utf8) {
+            file.write(trackpoint)
           }
 
-          file.write(
-            "<trkpt lat=\"\(location.coordinate.latitude)\" lon=\"\(location.coordinate.longitude)\"><ele>\(location.altitude.magnitude)</ele><time>\(iso_formatter.string(from: location.timestamp))</time>\(hr_string)</trkpt>"
-              .data(using: .utf8)!
-          )
         }
         if (done){
-          file.write("</trkseg></trk></gpx>".data(using: .utf8)!)
+          file.write("""
+    </trkseg>
+  </trk>
+</gpx>
+""".data(using: .utf8)!)
           file.closeFile()
 
           let activityViewController = UIActivityViewController(
@@ -215,12 +218,51 @@ class WorkoutsTableViewController: UITableViewController {
     }
   }
 
-  func stringFromTimeInterval(interval: Double) -> String {
+  private func stringFromTimeInterval(interval: Double) -> String {
 
     let hours = (Int(interval) / 3600)
     let minutes = Int(interval / 60) - Int(hours * 60)
     let seconds = Int(interval) - (Int(interval / 60) * 60)
 
     return String(format: "%0.2d:%0.2d:%0.2d",hours,minutes,seconds)
+  }
+
+  private func gpxTrackPoint(location: CLLocation, hr_string: String) -> String {
+    let iso_formatter = ISO8601DateFormatter()
+
+    return """
+      <trkpt lat=\"\(location.coordinate.latitude)" lon="\(location.coordinate.longitude)">
+        <ele>\(location.altitude.magnitude)</ele>
+        <time>\(iso_formatter.string(from: location.timestamp))</time>        \(hr_string)
+      </trkpt>"
+
+"""
+  }
+
+  private func gpxHeartRate(current_hr: Double) -> String {
+    return """
+
+        <extensions>
+          <gpxtpx:TrackPointExtension>
+            <gpxtpx:hr>\(current_hr)</gpxtpx:hr>
+          </gpxtpx:TrackPointExtension>
+        </extensions>
+"""
+  }
+  private func gpxHeader(title: String, startDate: Date) -> String {
+    let iso_formatter = ISO8601DateFormatter()
+
+    return """
+<?xml version="1.0" encoding="UTF-8"?>
+<gpx creator="StravaGPX" version="1.1" xmlns="http://www.topografix.com/GPX/1/1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd http://www.garmin.com/xmlschemas/GpxExtensions/v3 http://www.garmin.com/xmlschemas/GpxExtensionsv3.xsd http://www.garmin.com/xmlschemas/TrackPointExtension/v1 http://www.garmin.com/xmlschemas/TrackPointExtensionv1.xsd" xmlns:gpxtpx="http://www.garmin.com/xmlschemas/TrackPointExtension/v1" xmlns:gpxx="http://www.garmin.com/xmlschemas/GpxExtensions/v3">
+
+  <metadata>
+    <time>\(iso_formatter.string(from: startDate))</time>
+  </metadata>
+  <trk>
+    <name>\(title)</name>
+    <trkseg>
+
+"""
   }
 }
