25aa6814873b306f86626f4fca28de5bf9ad68c4
Mario Martelli <cocoa@schnuddelhuddel.de>
2017-11-25 14:08:15 +0100

    Identation is changed for tableview controller

--- GpxExport/WorkoutsTableViewController.swift
+++ GpxExport/WorkoutsTableViewController.swift
@@ -9,9 +9,9 @@ class WorkoutsTableViewController: UITableViewController {
     case finishedCreatingWorkout
   }
 
-    lazy private var workoutStore: WorkoutDataStore = {
-        return WorkoutDataStore()
-    }()
+  lazy private var workoutStore: WorkoutDataStore = {
+    return WorkoutDataStore()
+  }()
 
   private var workouts: [HKWorkout]?
 
@@ -76,100 +76,100 @@ class WorkoutsTableViewController: UITableViewController {
   }
 
 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath)
-        guard let workouts = self.workouts else {
-            return
-        }
-
-        if (indexPath.row >= workouts.count){
-            return
-        }
-
-        print(indexPath.row)
-        let workout = workouts[indexPath.row]
-        let workout_name: String = {
-            switch workout.workoutActivityType {
-                case .cycling: return "Cycle"
-                case .running: return "Run"
-                case .walking: return "Walk"
-                default: return "Workout"
-            }
-        }()
-        let workout_title = "\(workout_name) - \(self.dateFormatter.string(from: workout.startDate))"
-        let file_name = "\(self.filenameDateFormatter.string(from: workout.startDate)) - \(workout_name)"
-
-        let targetURL = URL(fileURLWithPath: NSTemporaryDirectory())
-            .appendingPathComponent(file_name)
-            .appendingPathExtension("gpx")
-
-        let file: FileHandle
-
-        do {
-            let manager = FileManager.default
-            if manager.fileExists(atPath: targetURL.path){
-                try manager.removeItem(atPath: targetURL.path)
-            }
-            print(manager.createFile(atPath: targetURL.path, contents: Data()))
-            file = try FileHandle(forWritingTo: targetURL)
-        }catch let err {
-            print(err)
-            return
-        }
-
-        workoutStore.heartRate(for: workouts[indexPath.row]){
-            (rates, error) in
-
-            guard let keyedRates = rates, error == nil else {
-                print(error as Any)
-                return
-            }
-
-            let iso_formatter = ISO8601DateFormatter()
-            var current_heart_rate_index = 0
-            var current_hr: Double = -1
-            let bpm_unit = HKUnit(from: "count/min")
-            var hr_string = ""
-            file.write(
-                "<?xml version=\"1.0\" encoding=\"UTF-8\"?><gpx version=\"1.1\" creator=\"Apple Workouts (via pilif's hack of the week)\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.topografix.com/GPX/1/1\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\" xmlns:gpxtpx=\"http://www.garmin.com/xmlschemas/TrackPointExtension/v1\"><trk><name><![CDATA[\(workout_title)]]></name><time>\(iso_formatter.string(from: workout.startDate))</time><trkseg>"
-                        .data(using: .utf8)!
-            )
-
-            self.workoutStore.route(for: workouts[indexPath.row]){
-                (maybe_locations, done, error) in
-                guard let locations = maybe_locations, error == nil else {
-                    print(error as Any)
-                    file.closeFile()
-                    return
-                }
-
-                for location in locations {
-                    while (current_heart_rate_index < keyedRates.count) && (location.timestamp > keyedRates[current_heart_rate_index].startDate) {
-                        current_hr = keyedRates[current_heart_rate_index].quantity.doubleValue(for: bpm_unit)
-                        current_heart_rate_index += 1
-                        hr_string = "<extensions><gpxtpx:TrackPointExtension><gpxtpx:hr>\(current_hr)</gpxtpx:hr></gpxtpx:TrackPointExtension></extensions>"
-                    }
-
-                    file.write(
-                        "<trkpt lat=\"\(location.coordinate.latitude)\" lon=\"\(location.coordinate.longitude)\"><ele>\(location.altitude.magnitude)</ele><time>\(iso_formatter.string(from: location.timestamp))</time>\(hr_string)</trkpt>"
-                            .data(using: .utf8)!
-                    )
-                }
-                if (done){
-                    file.write("</trkseg></trk></gpx>".data(using: .utf8)!)
-                    file.closeFile()
-
-                    let activityViewController = UIActivityViewController(
-                        activityItems: [targetURL],
-                        applicationActivities: nil)
-                    if let popoverPresentationController = activityViewController.popoverPresentationController {
-                        popoverPresentationController.barButtonItem = nil
-                    }
-                    self.present(activityViewController, animated: true, completion: nil)
-                }
-            }
-        }
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    print(indexPath)
+    guard let workouts = self.workouts else {
+      return
     }
+
+    if (indexPath.row >= workouts.count){
+      return
+    }
+
+    print(indexPath.row)
+    let workout = workouts[indexPath.row]
+    let workout_name: String = {
+      switch workout.workoutActivityType {
+      case .cycling: return "Cycle"
+      case .running: return "Run"
+      case .walking: return "Walk"
+      default: return "Workout"
+      }
+    }()
+    let workout_title = "\(workout_name) - \(self.dateFormatter.string(from: workout.startDate))"
+    let file_name = "\(self.filenameDateFormatter.string(from: workout.startDate)) - \(workout_name)"
+
+    let targetURL = URL(fileURLWithPath: NSTemporaryDirectory())
+      .appendingPathComponent(file_name)
+      .appendingPathExtension("gpx")
+
+    let file: FileHandle
+
+    do {
+      let manager = FileManager.default
+      if manager.fileExists(atPath: targetURL.path){
+        try manager.removeItem(atPath: targetURL.path)
+      }
+      print(manager.createFile(atPath: targetURL.path, contents: Data()))
+      file = try FileHandle(forWritingTo: targetURL)
+    }catch let err {
+      print(err)
+      return
+    }
+
+    workoutStore.heartRate(for: workouts[indexPath.row]){
+      (rates, error) in
+
+      guard let keyedRates = rates, error == nil else {
+        print(error as Any)
+        return
+      }
+
+      let iso_formatter = ISO8601DateFormatter()
+      var current_heart_rate_index = 0
+      var current_hr: Double = -1
+      let bpm_unit = HKUnit(from: "count/min")
+      var hr_string = ""
+      file.write(
+        "<?xml version=\"1.0\" encoding=\"UTF-8\"?><gpx version=\"1.1\" creator=\"Apple Workouts (via pilif's hack of the week)\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns=\"http://www.topografix.com/GPX/1/1\" xsi:schemaLocation=\"http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd\" xmlns:gpxtpx=\"http://www.garmin.com/xmlschemas/TrackPointExtension/v1\"><trk><name><![CDATA[\(workout_title)]]></name><time>\(iso_formatter.string(from: workout.startDate))</time><trkseg>"
+          .data(using: .utf8)!
+      )
+
+      self.workoutStore.route(for: workouts[indexPath.row]){
+        (maybe_locations, done, error) in
+        guard let locations = maybe_locations, error == nil else {
+          print(error as Any)
+          file.closeFile()
+          return
+        }
+
+        for location in locations {
+          while (current_heart_rate_index < keyedRates.count) && (location.timestamp > keyedRates[current_heart_rate_index].startDate) {
+            current_hr = keyedRates[current_heart_rate_index].quantity.doubleValue(for: bpm_unit)
+            current_heart_rate_index += 1
+            hr_string = "<extensions><gpxtpx:TrackPointExtension><gpxtpx:hr>\(current_hr)</gpxtpx:hr></gpxtpx:TrackPointExtension></extensions>"
+          }
+
+          file.write(
+            "<trkpt lat=\"\(location.coordinate.latitude)\" lon=\"\(location.coordinate.longitude)\"><ele>\(location.altitude.magnitude)</ele><time>\(iso_formatter.string(from: location.timestamp))</time>\(hr_string)</trkpt>"
+              .data(using: .utf8)!
+          )
+        }
+        if (done){
+          file.write("</trkseg></trk></gpx>".data(using: .utf8)!)
+          file.closeFile()
+
+          let activityViewController = UIActivityViewController(
+            activityItems: [targetURL],
+            applicationActivities: nil)
+          if let popoverPresentationController = activityViewController.popoverPresentationController {
+            popoverPresentationController.barButtonItem = nil
+          }
+          self.present(activityViewController, animated: true, completion: nil)
+        }
+      }
+    }
+  }
 
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
@@ -198,25 +198,24 @@ class WorkoutsTableViewController: UITableViewController {
     return cell
   }
 
-    private func authorizeHealthKit() {
+  private func authorizeHealthKit() {
 
-        HealthKitSetupAssistant.authorizeHealthKit { (authorized, error) in
+    HealthKitSetupAssistant.authorizeHealthKit { (authorized, error) in
 
-            guard authorized else {
+      guard authorized else {
 
-                let baseMessage = "HealthKit Authorization Failed"
+        let baseMessage = "HealthKit Authorization Failed"
 
-                if let error = error {
-                    print("\(baseMessage). Reason: \(error.localizedDescription)")
-                } else {
-                    print(baseMessage)
-                }
-
-                return
-            }
-
-            print("HealthKit Successfully Authorized.")
+        if let error = error {
+          print("\(baseMessage). Reason: \(error.localizedDescription)")
+        } else {
+          print(baseMessage)
         }
 
+        return
+      }
+
+      print("HealthKit Successfully Authorized.")
     }
+  }
 }
