7eaf759b36ae8613d302b583be3fa51b3878b8a6
Mario Martelli <cocoa@schnuddelhuddel.de>
2019-03-03 07:44:54 +0100

    fit writing routine is refactored

--- GpxExport/Models/Workout+FitWriter.swift
+++ GpxExport/Models/Workout+FitWriter.swift
@@ -14,9 +14,6 @@ import FitnessUnits
 
 extension Workout {
     func writeFit() -> URL? {
-        var currentHeartrateIndex = 0
-        var currentHeartrate: Double = -1
-        let bpmUnit = HKUnit(from: "count/min")
 
         let formatter = DateFormatter()
         formatter.dateFormat = "yyyy-MM-dd HH.mm.ss"
@@ -42,74 +39,13 @@ extension Workout {
                 return
             }
 
-            let time = FitTime(date: Date())
-            let duration = Measurement(value: self.duration, unit: UnitDuration.seconds)
             var messages: [FitMessage] = []
-            let activity = ActivityMessage(timeStamp: time,
-                                           totalTimerTime: duration,
-                                           localTimeStamp: nil,
-                                           numberOfSessions: nil,
-                                           activity: Activity.manual,
-                                           event: nil,
-                                           eventType: nil,
-                                           eventGroup: nil)
-            messages.append(activity)
-            let fileId = FileIdMessage(deviceSerialNumber: nil,
-                                        fileCreationDate: time,
-                                        manufacturer: .garmin,
-                                        product: nil,
-                                        fileNumber: nil,
-                                        fileType: FileType.activity,
-                                        productName: nil)
-
-            let stanceTime = StanceTime.nilSelf
-            let torqueEffectiveness = TorqueEffectiveness.nilSelf
-            let pedalSmoothness = PedalSmoothness.nilSelf
-
-            for location in self.route {
-                var heartrate: UInt8 = 0
-                while (currentHeartrateIndex < self.heartRate.count) && (location.timestamp > self.heartRate[currentHeartrateIndex].startDate) {
-                    currentHeartrate = self.heartRate[currentHeartrateIndex].quantity.doubleValue(for: bpmUnit)
-                    currentHeartrateIndex += 1
-                    heartrate = UInt8(currentHeartrate)
-                }
-                let latitude = ValidatedMeasurement(value: location.coordinate.latitude, valid: true, unit: UnitAngle.degrees)
-                let longitude = ValidatedMeasurement(value: location.coordinate.longitude, valid: true, unit: UnitAngle.degrees)
-                let altitude = ValidatedMeasurement(value: location.altitude, valid: true, unit: UnitLength.meters)
-                let record = RecordMessage(timeStamp: FitTime(date: location.timestamp),
-                                           position: Position(latitude: latitude, longitude: longitude),
-                                           distance: nil,
-                                           timeFromCourse: nil,
-                                           cycles: nil,
-                                           totalCycles: nil,
-                                           accumulatedPower: nil,
-                                           altitude: altitude,
-                                           speed: nil,
-                                           power: nil,
-                                           gpsAccuracy: nil,
-                                           verticalSpeed: nil,
-                                           calories: nil,
-                                           verticalOscillation: nil,
-                                           stanceTime: stanceTime,
-                                           heartRate: heartrate,
-                                           cadence: nil,
-                                           grade: nil,
-                                           resistance: nil,
-                                           cycleLength: nil,
-                                           temperature: nil,
-                                           activity: nil,
-                                           torqueEffectiveness: torqueEffectiveness,
-                                           pedalSmoothness: pedalSmoothness,
-                                           stroke: nil,
-                                           zone: nil,
-                                           ballSpeed: nil,
-                                           deviceIndex: nil)
-                messages.append(record)
-            }
+            messages.append(self.createActivityMessage())
+            messages += self.createRecords()
 
             do {
                 let encoder = FitFileEncoder(dataValidityStrategy: .none)
-                let data = try encoder.encode(fildIdMessage: fileId, messages: messages)
+                let data = try encoder.encode(fildIdMessage: self.createFileId(), messages: messages)
                 file.write(data)
 
             } catch {
@@ -117,5 +53,67 @@ extension Workout {
             }
         }
         return targetURL
+    }
+
+    func createActivityMessage() -> ActivityMessage {
+        let time = FitTime(date: Date())
+        let duration = Measurement(value: self.duration, unit: UnitDuration.seconds)
+        let activity = ActivityMessage(timeStamp: time,
+                                       totalTimerTime: duration,
+                                       localTimeStamp: nil,
+                                       numberOfSessions: nil,
+                                       activity: Activity.manual,
+                                       event: nil,
+                                       eventType: nil,
+                                       eventGroup: nil)
+        return activity
+    }
+
+    func createFileId() -> FileIdMessage {
+        let time = FitTime(date: Date())
+        let fileId = FileIdMessage(deviceSerialNumber: nil,
+                                   fileCreationDate: time,
+                                   manufacturer: .garmin,
+                                   product: nil,
+                                   fileNumber: nil,
+                                   fileType: FileType.activity,
+                                   productName: nil)
+        return fileId
+
+    }
+
+    func createRecords() -> [FitMessage] {
+
+        var currentHeartrateIndex = 0
+        var currentHeartrate: Double = -1
+        let bpmUnit = HKUnit(from: "count/min")
+        var records: [RecordMessage] = []
+
+        let stanceTime = StanceTime.nilSelf
+        let torqueEffectiveness = TorqueEffectiveness.nilSelf
+        let pedalSmoothness = PedalSmoothness.nilSelf
+
+        for location in self.route {
+            var heartrate: UInt8 = 0
+            while (currentHeartrateIndex < self.heartRate.count) && (location.timestamp > self.heartRate[currentHeartrateIndex].startDate) {
+                currentHeartrate = self.heartRate[currentHeartrateIndex].quantity.doubleValue(for: bpmUnit)
+                currentHeartrateIndex += 1
+                heartrate = UInt8(currentHeartrate)
+            }
+            let latitude = ValidatedMeasurement(value: location.coordinate.latitude, valid: true, unit: UnitAngle.degrees)
+            let longitude = ValidatedMeasurement(value: location.coordinate.longitude, valid: true, unit: UnitAngle.degrees)
+            let altitude = ValidatedMeasurement(value: location.altitude, valid: true, unit: UnitLength.meters)
+            let record = RecordMessage(timeStamp: FitTime(date: location.timestamp),
+                                       position: Position(latitude: latitude, longitude: longitude),
+                                       distance: nil, timeFromCourse: nil, cycles: nil, totalCycles: nil,
+                                       accumulatedPower: nil, altitude: altitude, speed: nil, power: nil,
+                                       gpsAccuracy: nil, verticalSpeed: nil, calories: nil, verticalOscillation: nil,
+                                       stanceTime: stanceTime, heartRate: heartrate, cadence: nil, grade: nil,
+                                       resistance: nil, cycleLength: nil, temperature: nil, activity: nil,
+                                       torqueEffectiveness: torqueEffectiveness, pedalSmoothness: pedalSmoothness,
+                                       stroke: nil, zone: nil, ballSpeed: nil, deviceIndex: nil)
+            records.append(record)
+        }
+        return records
     }
 }
